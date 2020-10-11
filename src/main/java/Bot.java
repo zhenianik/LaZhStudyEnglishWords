36adfa7edcb4448333fa5979227d0ee77010f590
zhenianik <dannilkaaa@gmail.com>
2020-10-11 16:00:52 +0300

    добавил команды для работы с mysql

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -11,19 +11,19 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 public class Bot extends TelegramLongPollingBot {
 
     private static Connection myCon = null;
-    private boolean connectionStatus = SetConnection();
+    private static String currentWord = "";
+    private static HashMap<String, String> properties = getProperties();
 
     public static void main(String[] args) throws SQLException {
+
+        SetConnection(properties.get("url"), properties.get("username"), properties.get("password"));
 
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
@@ -34,7 +34,9 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
-    private boolean SetConnection() {
+    private static HashMap<String, String> getProperties() {
+        HashMap<String, String> properties = new HashMap<>();
+
         Properties props = new Properties();
         try {
             FileInputStream in = new FileInputStream("C:/java/db.properties");
@@ -47,43 +49,80 @@ public class Bot extends TelegramLongPollingBot {
         String driver = props.getProperty("jdbc.driver");
         if (driver != null) {
             try {
-                Class.forName(driver) ;
+                Class.forName(driver);
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             }
         }
 
-        String url = props.getProperty("jdbc.url");
-        String username = props.getProperty("jdbc.username");
-        String password = props.getProperty("jdbc.password");
+        properties.put("url", props.getProperty("jdbc.url"));
+        properties.put("username", props.getProperty("jdbc.username"));
+        properties.put("password", props.getProperty("jdbc.password"));
+        properties.put("telegramBotToken", props.getProperty("telegramBotToken"));
+        properties.put("telegramBotUsername", props.getProperty("telegramBotUsername"));
+
+        return properties;
+    }
+
+    private static void SetConnection(String url, String username, String password) {
 
         try {
             myCon = DriverManager.getConnection(url, username, password);
-            return true;
         } catch (SQLException throwables) {
             //System.out.println("don't work");
             throwables.printStackTrace();
-            return false;
         }
     }
 
     public void onUpdateReceived(Update update) {
+        ArrayList<String> buttonList = new ArrayList<>();
+        buttonList.add("/test");
+        buttonList.add("/video");
+
         Message message = update.getMessage();
         if (message != null && message.hasText()) {
             String text = message.getText();
-            if ("/help".equals(text)) {
-                sendMsg(message, "Чем я могу помочь", true);
-            } else if ("/setting".equals(text)) {
-                sendMsg(message, "Что будем настраивать", true);
+            if ("/test".equals(text)) {
+                ArrayList<String> answer = getRequest(getRandomWord());
+                sendMsg(message, answer.toString(), true, buttonList);
             } else if ("/video".equals(text)) {
-                sendMsg(message, "https://youtu.be/m-5RSSZg0Os", false);
+                ArrayList<String> answer = getRequest(getRandomVideo());
+                for (String str : answer) {
+                    String w[] = str.split(";");
+                    sendMsg(message, w.toString(), false, buttonList);
+                }
+            } else if ("/yes".equals(text)) {
+                boolean answer = getRequestInsert(addNewWord(currentWord, message.getFrom().getUserName()));
+                if (answer) sendMsg(message, "Слово успешно добавлено", false, buttonList);
+                else sendMsg(message, "Что-то пошло не так, слово не добавлено.", false, buttonList);
+            } else if ("/no".equals(text)) {
+                sendMsg(message, "Хорошо, не будем добавлять.", false, buttonList);
             } else {
-                sendMsg(message, "А не пошёл бы ты в жопу?!", true);
+                // если ввели слово (не команда), тогда проверим его наличие в бд и выведем соотв.результат
+                ArrayList<String> answer = getRequest(chekWord(text));
+                if (answer.size() != 0) {
+                    for (String str : answer) {
+                        String arr[] = str.split(";");
+                        String mystr = "перевод: ";
+                        for (int i = 1; i < arr.length - 1; i++) {
+                            String s = arr[i + 1].equals("") ? "" : ", ";
+                            if (!arr[i].equals("")) mystr = mystr + arr[i] + s;
+                        }
+                        sendMsg(message, mystr, true, buttonList);
+                    }
+                } else {
+                    ArrayList<String> buttonListAdd = new ArrayList<>();
+                    buttonListAdd.add("/yes");
+                    buttonListAdd.add("/no");
+                    sendMsg(message, "Такого слова нет в словаре! Добавить?", true, buttonListAdd);
+                    currentWord = text;
+                }
+
             }
         }
     }
 
-    public void setButtons(SendMessage sendMessage) {
+    public void setButtons(SendMessage sendMessage, ArrayList<String> buttonList) {
         ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
         sendMessage.setReplyMarkup(replyKeyboardMarkup);
         replyKeyboardMarkup.setSelective(true);
@@ -93,15 +132,15 @@ public class Bot extends TelegramLongPollingBot {
         List<KeyboardRow> keyboardRowList = new ArrayList<KeyboardRow>();
         KeyboardRow keyboarfirstrow = new KeyboardRow();
 
-        keyboarfirstrow.add(new KeyboardButton("/help"));
-        keyboarfirstrow.add(new KeyboardButton("/setting"));
-        keyboarfirstrow.add(new KeyboardButton("/video"));
+        for (String s : buttonList) {
+            keyboarfirstrow.add(new KeyboardButton(s));
+        }
 
         keyboardRowList.add(keyboarfirstrow);
         replyKeyboardMarkup.setKeyboard(keyboardRowList);
     }
 
-    public void sendMsg(Message message, String text, boolean enableMarkdown) {
+    public void sendMsg(Message message, String text, boolean enableMarkdown, ArrayList<String> buttonList) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.enableMarkdown(enableMarkdown);
         sendMessage.setChatId(message.getChatId().toString());
@@ -109,7 +148,7 @@ public class Bot extends TelegramLongPollingBot {
         sendMessage.setText(text);
 
         try {
-            setButtons(sendMessage);
+            setButtons(sendMessage, buttonList);
             execute(sendMessage);
         } catch (TelegramApiException e) {
             e.printStackTrace();
@@ -118,25 +157,59 @@ public class Bot extends TelegramLongPollingBot {
     }
 
     public String getBotUsername() {
-        return "LaZhStudyEnglishWords_bot";
+        return properties.get("telegramBotUsername");
     }
 
     public String getBotToken() {
-        return "1175991949:AAFgsPgTp5yKCFV0J1grvL8E-mVNN4vYbWc";
+        return properties.get("telegramBotToken");
     }
 
-    public String getRequest() {
+    public static ArrayList<String> getRequest(String sqlText) {
+        ArrayList<String> result = new ArrayList<String>();
         Statement myStmt = null;
         try {
+            if (myCon.isClosed()) SetConnection(properties.get("url"), properties.get("username"), properties.get("password"));
             myStmt = myCon.createStatement();
-            String sql = "select * from srv30737_english.words";
-            ResultSet rs = myStmt.executeQuery(sql);
+            ResultSet rs = myStmt.executeQuery(sqlText);
             while (rs.next()) {
-                System.out.println(rs.getString("word"));
+                result.add(rs.getString("word") + ";" + rs.getString("translate1") +
+                        ";" + rs.getString("translate2") + ";" + rs.getString("translate3") +
+                        ";" + rs.getString("translate4") + ";" + rs.getString("context"));
             }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-        return "";
+        return result;
     }
+
+    public static boolean getRequestInsert(String sqlText) {
+        Statement myStmt = null;
+        try {
+            myStmt = myCon.createStatement();
+            myStmt.executeUpdate(sqlText);
+            return true;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return false;
+        }
+    }
+
+    public static String getRandomVideo() {
+        return "SELECT DISTINCT word, translate1, translate2, translate3, translate4, context from words order by RAND() LIMIT 1";
+    }
+
+    public static String getRandomWord() {
+        return "SELECT word, translate1, translate2, translate3, translate4, context from words order by RAND() LIMIT 10";
+    }
+
+    public static String chekWord(String text) {
+        return "SELECT word, translate1, translate2, translate3, translate4, context from words where word = '" + text + "'";
+    }
+
+    public static String addNewWord(String text, String context) {
+        return "INSERT INTO `words` (`id_word`, `word`, `translate1`, `translate2`, `translate3`, `translate4`, `context`) " +
+                "VALUES (NULL, '" + text + "', '', '', '', '', '" + context + "')";
+    }
+
 }
+
